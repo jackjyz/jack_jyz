f146d739d5d6295b0dfa901dc8d92f8a7fd981fc
JiaYongZhuo <1549884311@qq.com>
2019-09-03 18:00:27 +0800

    修改为restful风格访问

--- maven_test/src/main/java/com/zzti/controller/UserController.java
+++ maven_test/src/main/java/com/zzti/controller/UserController.java
@@ -5,6 +5,8 @@ import com.zzti.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +15,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/1")
-    public String findUsers(Model model){
-        User userById = userService.findUserById(1);
+    @GetMapping("/{userId}")
+    public String findUsers(Model model,@PathVariable("userId") Integer userId){
+        User userById = userService.findUserById(userId);
         model.addAttribute("item",userById);
 
         return "index";
